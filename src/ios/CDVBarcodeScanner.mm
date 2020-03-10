da884fe56788fb802ce00064820d1b5d18ee92e5
Florian Brüggemann <florian@prototype.berlin>
2020-03-10 17:22:10 +0100

    Disable pulldown on ios 13

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -352,6 +352,9 @@ parentViewController:(UIViewController*)parentViewController
     // here we set the orientation delegate to the MainViewController of the app (orientation controlled in the Project Settings)
     self.viewController.orientationDelegate = self.plugin.viewController;
 
+    // Disable sheet pull down
+    self.viewController.modalInPresentation = YES;
+
     // delayed [self openDialog];
     [self performSelector:@selector(openDialog) withObject:nil afterDelay:1];
 }
